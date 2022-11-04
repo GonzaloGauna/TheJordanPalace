86c16e05ec982835c0ec358359f396bee745fb0a
mateQF <mateo.fortu@gmail.com>
2022-11-04 18:21:01 -0300

    slider

---
+++ slider/slick-1.8.1/carrusel.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".carrusel").slick({
+    adaptiveHeight: true,
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    mobileFirst: true
+  });
+});
