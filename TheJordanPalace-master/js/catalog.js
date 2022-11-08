409243270dc808c7935cd7ed053c92a5ceec3317
SantyPizarro <113479103+SantyPizarro@users.noreply.github.com>
2022-11-08 03:36:56 -0300

    Delete TheJordanPalace-master directory

--- TheJordanPalace-master/js/catalog.js
+++
@@ -1,9 +0,0 @@
-const filterBtn = document.getElementById("filterBtn");
-
-filterBtn.addEventListener("click", () => {
-    const filtersContainer = document.getElementById("filtersContainer");
-
-    filtersContainer.classList.toggle("filters-close");
-    filtersContainer.classList.toggle("filters__container--close");
-    filtersContainer.classList.toggle("filters__container--open");
-})
