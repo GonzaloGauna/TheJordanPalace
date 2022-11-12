403a3251ffedcd223c7646d37bddac413502fe15
gonzaloherlein <gherlein@alumno.unlam.edu.ar>
2022-11-12 13:59:22 -0300

    deshabilitando el boton

--- js/login.js
+++ js/login.js
@@ -1,16 +1,25 @@
 const form = document.querySelector("form");
-
 let listaDeUsuariosRegistrados = JSON.parse(localStorage.getItem("Usuarios"));
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let button = document.getElementById('button');
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault;
+    enableButton(password);
     validation();
 })
 
-function validation() {
-    let email = document.querySelector("#email");
-    let password = document.querySelector("#password");
+function enableButton(password) {
+    if(password.value != ''){
+        button.disabled = false;
+    }else{
+        button.disabled = true;
+    }
+}
 
+
+function validation() {
     listaDeUsuariosRegistrados.forEach( usuario =>{
         if(usuario.email == email.value && usuario.password == password.value){
             alert('Ingresando....');
