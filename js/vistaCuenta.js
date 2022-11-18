29dd4c400df4c8277767691b91abcef9260be1ea
mateQF <mateo.fortu@gmail.com>
2022-11-18 15:34:39 -0300

    js variables

--- js/vistaCuenta.js
+++ js/vistaCuenta.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const agregarTarjeta = document.getElementById("agregarTarjeta");
 const popupTarjeta = document.getElementById("popupTarjeta");
@@ -7,29 +7,36 @@ const popupDireccion = document.getElementById("popupDireccion");
 const agregarDireccion = document.getElementById("agregarDireccion");
 const closeBtnDireccion = document.getElementById("closeBtnDireccion");
 
-// formulario
-const formulario =  document.querySelector('#form-tarjeta')
-const numeroTarjeta = document.querySelector('#serial')
-const codigoSeguridad = document.querySelector('#cod-seguridad')
-const vencimiento = document.querySelector('#vencimiento')
-const titular = document.querySelector('#titular')
+// campos de formulario
+const formulario = document.querySelector("#form-tarjeta");
+const numeroTarjeta = document.querySelector("#serial");
+const codigoSeguridad = document.querySelector("#cod-seguridad");
+const vencimiento = document.querySelector("#vencimiento");
+const titular = document.querySelector("#titular");
+
+// botones de agregar
+const btnAgregarDireccion = document.querySelector("#agregarDireccionPopUp");
+const btnGuardarTarjeta = document.querySelector("#guardarTarjeta");
+
+// botones de eliminar
+const botonesEliminar = document.getElementsByClassName(".eliminar");
 
 // arrays
 const tarjetas = [];
 const direcciones = [];
 
-agregarTarjeta.addEventListener("click", ()=> {
-    popupTarjeta.classList.add("popupTarjetasActivo");
-})
+agregarTarjeta.addEventListener("click", () => {
+  popupTarjeta.classList.add("popupTarjetasActivo");
+});
 
-closeBtnTarjeta.addEventListener("click", ()=> {
-    popupTarjeta.classList.remove("popupTarjetasActivo");
-})
+closeBtnTarjeta.addEventListener("click", () => {
+  popupTarjeta.classList.remove("popupTarjetasActivo");
+});
 
-agregarDireccion.addEventListener("click", ()=> {
-    popupDireccion.classList.add("popUpDireccionActivo");
-})
+agregarDireccion.addEventListener("click", () => {
+  popupDireccion.classList.add("popUpDireccionActivo");
+});
 
-closeBtnDireccion.addEventListener("click", ()=> {
-    popupDireccion.classList.remove("popUpDireccionActivo");
-})
+closeBtnDireccion.addEventListener("click", () => {
+  popupDireccion.classList.remove("popUpDireccionActivo");
+});
